9398015bb2cdf5c3ddb28849ae2319670ace702c
Samuel <samuel.linehan15@gmail.com>
2016-06-01 14:20:58 -0400

    initial commit

---
+++ main.js
@@ -0,0 +1,27 @@
+// Problem 1
+
+// function findSum(number) {
+//   var sum = 0;
+//   for(var i = 0; i < number; i++) {
+//     if(i % 5 === 0) {
+//       sum += i;
+//     } else if(i % 3 === 0) {
+//       sum += i;
+//     }
+//   }
+//   return sum;
+// }
+//
+// console.log(findSum(1000));
+
+
+// Problem 2
+
+function fibonacci(number) {
+  var sum = 0;
+  for(var i = 0; i < number; i++) {
+    if(sum < 4000000) {
+      
+    }
+  }
+}
